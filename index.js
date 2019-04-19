4a4cdea8360c57bda4de5b3d3341f9cb981362ae
shiratap <spencerhirata@gmail.com>
2019-04-19 09:20:16 -0700

    no faker

--- index.js
+++ index.js
@@ -3,9 +3,6 @@
 // HARD CODED PORT! DO NOT CHANGE
 const PORT = 4444;
 
-//We're taking the node module faker hacker property
-const { phrase } = require('faker').hacker;
-
 // `io` is your server instantiated with the port
 const io = require('socket.io')(PORT);
 
@@ -16,18 +13,23 @@ io.on('connection', socket => {
   console.log(`${socket.id} connected`);
   
   //A welcome message for whoever joins
-  const welcome = `Hello there ${socket.id} from server`;
+  const welcome = `Hello there ${socket.id} from template server. This is a number guessing game. Guess a number between 1-9`;
   socket.emit('output', welcome);
   
   /*
   The client will only emit an event('input', line), where line is what the client typed in. The client emits NO OTHER EVENTS
-  This simple listener will merely console log the string below, along with the user input to the server terminal. It will then create a hacker phrase made from faker, and then emit the output event with the string below
+  This is your app logic in this below function. Currently, this game is just a guessing game. Guess between a number between 1-10 You can build hangman with the client clear function to repaint the hung man and the letters. You can do a number guessing game. This is your job.
   */
   socket.on('input', line => {
     console.log('A line we can work with:', line);
-
-    const payload = phrase();
-    socket.emit('output', `You said this: ${line}, but the server said this: ${payload}`)
+    const num = Math.floor(Math.random()*10);
+    console.log(num)
+    if(num == line){
+      socket.emit('output', 'YOU GUESSED MY NUMBER');
+    }
+    else{
+      socket.emit('output', 'You did not guess my number');
+    }
   });
 
   //Here, upon client disconnect, your server will log which socket disconnected.
