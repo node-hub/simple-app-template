7a36d8ac8cbb2dc25e787ade15986ae099fdf3d5
shiratap <spencerhirata@gmail.com>
2019-04-19 12:04:10 -0700

    log updates

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const io = require('socket.io')(PORT);
 
 // When sockets connect to your server
 io.on('connection', socket => {
-  
+  socket.emit('clear', '');
   // A proof of life statement that console logs to your server terminal the socket id
   console.log(`${socket.id} connected`);
   
   //A welcome message for whoever joins
-  const welcome = `Hello there ${socket.id} from template server. This is a number guessing game. Guess a number between 1-9`;
+  const welcome = `Hello there ${socket.id} from template server. This is a number guessing game. Guess a number between 0-9`;
   socket.emit('output', welcome);
   
   /*
@@ -21,7 +21,7 @@ io.on('connection', socket => {
   This is your app logic in this below function. Currently, this game is just a guessing game. Guess between a number between 1-10 You can build hangman with the client clear function to repaint the hung man and the letters. You can do a number guessing game. This is your job.
   */
   socket.on('input', line => {
-    console.log('A line we can work with:', line);
+    console.log('User Input:', line);
     const num = Math.floor(Math.random()*10);
     console.log(num)
     if(num == line){
